c9fced8f7c527ed2af6e62b33d9d569160040fb8
Avonts <vinialves022@gmail.com>
2020-10-04 12:03:03 -0300

    Clients endpoints ok

---
+++ Exbackend/src/main/resources/data.sql
@@ -0,0 +1,39 @@
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Vinícius Alves', '47012217938', 3000.00, NOW(), 0, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Etelvina Santos', '12393922816', 1800.00, NOW(), 2, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Roque dos Santos', '07807682881', 4200.00, NOW(), 1, NOW()); 
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Davi Soares', '12997682889', 3500.00, NOW(), 2, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Moana Flores', '68367788001', 6500.00, NOW(), 0, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Milena Nunes', '21276392001', 2600.00, NOW(), 1, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Rogerio Souza', '44088214030', 900.00, NOW(), 1, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Anderson Silva', '23435144025', 1700.00, NOW(), 0, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Luzia Antones', '66666084020', 1960.00, NOW(), 0, NOW());
+
+INSERT 
+  INTO tb_client (name, cpf, income, birth_date, children, created_At) 
+VALUES ('Eduarda Rizo', '84527019007', 4800.00, NOW(), 3, NOW());
